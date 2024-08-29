0db2acb0fd754e3af1ee19b9113d9b9f1c4d9577
Anna Ulyanova <ulyannava@gmail.com>
2024-08-29 23:02:39 +0200

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aulyanov <aulyanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 16:55:40 by aulyanov          #+#    #+#             */
+/*   Updated: 2024/08/28 20:34:33 by aulyanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	a;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		a = i + 1;
+		while (a < argc)
+		{
+			if (ft_strcmp(argv[i], argv[a]) > 0)
+				ft_swap(&argv[i], &argv[a]);
+			a++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
